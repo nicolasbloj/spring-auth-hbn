fe18cacae7c7e9ab53318c311bb3b1476354b14a
Nicolas Bloj <nicolasbloj@gmail.com>
2017-10-12 19:22:19 -0300

    Init -not Work

---
+++ src/main/java/com/auth0/samples/authapi/task/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.auth0.samples.authapi.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
+// The persistence layer of our application is backed by an in-memory database called HSQLDB.
+// este esquema de JPARepository y Entity Task no esta funcionando con spring mvc
